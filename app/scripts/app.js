038e18ac61f4087b7760eef3f4337de5afa443b0
chxiaoxin <e0001836@u.nus.edu>
2016-08-20 14:35:06 +0800

    add AngularJS form validation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -95,4 +95,92 @@ angular.module("angularController",[]).controller("MenuController",['$scope',fun
                                   }
                                   };
 }])
+
+.controller('dishDetailController',['$scope',function($scope) {
+
+            $scope.dish={
+                          name:'Uthapizza',
+                          image: 'images/uthapizza.png',
+                          category: 'mains', 
+                          label:'Hot',
+                          price:'4.99',
+                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                           comments: [
+                               {
+                                   rating:5,
+                                   comment:"Imagine all the eatables, living in conFusion!",
+                                   author:"John Lemon",
+                                   date:"2012-10-16T17:57:28.556Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                                   author:"Paul McVites",
+                                   date:"2014-09-05T17:57:28.556Z"
+                               },
+                               {
+                                   rating:3,
+                                   comment:"Eat it, just eat it!",
+                                   author:"Michael Jaikishan",
+                                   date:"2015-02-13T17:57:28.556Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Ultimate, Reaching for the stars!",
+                                   author:"Ringo Starry",
+                                   date:"2013-12-02T17:57:28.556Z"
+                               },
+                               {
+                                   rating:2,
+                                   comment:"It's your birthday, we're gonna party!",
+                                   author:"25 Cent",
+                                   date:"2011-12-02T17:57:28.556Z"
+                               }
+                               
+                           ]
+                    };
+            
+            
+            $scope.criteria='';
+            $scope.val='';
+            $scope.passVal=function(){
+                $scope.criteria=$scope.val;
+            };
+        }])
+.controller('FormController',['$scope',function($scope){
+    $scope.formval={
+        author:'',
+        rating:'',
+        comment:'',
+        date:''
+    };
+    $scope.invalidcheck=false;
+    $scope.commentSubmit=function(){
+        console.log($scope.formval);
+        if($scope.formval.rating==''){
+            $scope.invalidcheck=true;
+            console.log('incorrect');
+        }else{
+            var currentDate=new Date();
+            var year=currentDate.getFullYear();
+            var month=currentDate.getMonth()+1;
+            var date=currentDate.getDate();
+            var hour=currentDate.getHours();
+            var minute=currentDate.getMinutes();
+            var sec=currentDate.getSeconds();
+            var milsec=currentDate.getMilliseconds();
+            $scope.formval.date=year+'-'+month+'-'+date+'T'+hour+':'+minute+':'+sec+'.'+milsec+'Z';
+            $scope.dish.comments.push($scope.formval);
+            $scope.invalidcheck=false;
+            $scope.formval={
+        author:'',
+        rating:'',
+        comment:'',
+        date:''
+    };
+            $scope.formVal.$setPristine();
+            console.log($scope.formval);
+        }
+    };
+}])
 ;
